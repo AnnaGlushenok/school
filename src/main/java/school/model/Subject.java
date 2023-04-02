fa188c4a60e61d8a31deacb36c94b43795384384
annas <annasan14052003@gmail.com>
2023-04-02 23:29:47 +0300

    added subject model

---
+++ src/main/java/school/model/Subject.java
@@ -0,0 +1,57 @@
+package school.model;
+
+import jakarta.persistence.*;
+import lombok.NonNull;
+
+@Entity(name = "subjects")
+public class Subject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String path;
+    private String alt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getAlt() {
+        return alt;
+    }
+
+    public void setAlt(String alt) {
+        this.alt = alt;
+    }
+
+    public Subject() {
+
+    }
+
+    public Subject(Integer id, String name, String path, String alt) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+        this.alt = alt;
+    }
+}
