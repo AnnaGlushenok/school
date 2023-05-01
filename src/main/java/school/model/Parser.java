df0b90ea811f6b50963f8ee28ae92a64e324afc8
annas <annasan14052003@gmail.com>
2023-05-01 20:48:20 +0300

    added models

---
+++ src/main/java/school/model/Parser.java
@@ -0,0 +1,36 @@
+package school.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.minidev.json.JSONObject;
+import org.apache.tomcat.util.json.JSONParser;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Parser {
+    private String title;
+    private List<String> ids;
+    private List<StringBuilder> content;
+
+    public StringBuilder convertToJson() {
+        StringBuilder str = new StringBuilder("{");
+        for (int i = 0; i < ids.size(); i++)
+            str.append("\"")
+                    .append(ids.get(i))
+                    .append("\"")
+                    .append(":")
+                    .append("\"")
+                    .append(content.get(i))
+                    .append("\"")
+                    .append(",");
+        str.delete(str.length() - 1, str.length());
+        str.append("}");
+        return str;
+    }
+}
